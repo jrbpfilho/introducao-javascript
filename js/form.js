617e7fc110a8d03b31bfd0752a5a674324ad911c
Jose Ruy Borges <jrbpfilho@gmail.com>
2017-11-26 12:44:45 -0200

    My First Commit - Alura JavaScript for Web Course

---
+++ js/form.js
@@ -0,0 +1,111 @@
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var paciente = obtemPacienteDoFormulario(form);
+
+    var pacienteTr = montaTr(paciente);
+
+    var erros = validaPaciente(paciente);
+
+    if(erros.length > 0 ) {
+
+        exibeMensagemDeErro(erros);
+        return;
+    }
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+
+});
+
+function exibeMensagemDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+      erros.push("O nome não pode ser em branco");
+
+    }
+
+    if (paciente.gordura.length == 0) {
+      erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+      erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+      erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+      erros.push("Peso é invalido!");
+
+    }
+
+    if (!validaAltura(paciente.altura)) {
+      erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+function obtemPacienteDoFormulario(form) {
+
+    var paciente = {
+      nome: form.nome.value,
+      peso: form.peso.value,
+      altura: form.altura.value,
+      gordura: form.gordura.value,
+      imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
+
+function montaTr(paciente) {
+
+    //Cria TR
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    //Cria as TDs e a adiciona dentro da TR
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    // retorna a TR
+
+    return pacienteTr;
+}
